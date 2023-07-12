605a88dce985cb9dc9e4937438e9c4325723f942
Abhilash MJ <abhialash.jayaseelan@gmail.com>
2023-07-12 17:43:09 +0530

    changes to the socket url

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,6 +1,6 @@
 const configKeys = {
   API_URL: "https://talenthive.online/api/",
-  SOCKET_PORT: "ws://talenthive.online"
+  SOCKET_PORT: "https://talenthive.online"
 };
  
 export default configKeys;
